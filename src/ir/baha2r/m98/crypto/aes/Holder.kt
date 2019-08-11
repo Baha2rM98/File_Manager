c8bc20e8384a0c150e4ba06432d733e644656f57
Bahador <49520007+Baha2rM98@users.noreply.github.com>
2019-08-12 03:20:05 +0430

    access levels changed

--- src/ir/baha2r/m98/crypto/aes/Holder.kt
+++ src/ir/baha2r/m98/crypto/aes/Holder.kt
@@ -3,7 +3,8 @@ package ir.baha2r.m98.crypto.aes
 //TODO: add comments
 internal interface Holder {
     companion object {
-        val IV: ByteArray = byteArrayOf(19, -53, -47, -48, -75, 18, 13, 19, 19, 15, 19, -55, -56, 10, 19, 9)
-        val KEY: ByteArray = byteArrayOf(-55, 19, -51, -66, -63, -5, -2, 19, -47, -53, 19, -51, -53, -1, -75, -10)
+        internal val IV: ByteArray = byteArrayOf(19, -53, -47, -48, -75, 18, 13, 19, 19, 15, 19, -55, -56, 10, 19, 9)
+        internal val KEY: ByteArray =
+            byteArrayOf(-55, 19, -51, -66, -63, -5, -2, 19, -47, -53, 19, -51, -53, -1, -75, -10)
     }
 }
--- src/ir/baha2r/m98/crypto/aesfilemanager/MakeVisible.kt
+++ src/ir/baha2r/m98/crypto/aesfilemanager/MakeVisible.kt
@@ -5,7 +5,7 @@ internal object MakeVisible {
     private const val mul: Byte = -1
     private const val plus: Byte = 51
 
-    fun show(bytes: ByteArray): ByteArray {
+    internal fun show(bytes: ByteArray): ByteArray {
         val size = bytes.size
         val buf = ByteArray(size)
         for (i in 0 until size) {
