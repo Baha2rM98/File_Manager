ce9f00f6136073c200b012f9e635b798249200bb
Bahador <49520007+Baha2rM98@users.noreply.github.com>
2019-08-12 02:55:30 +0430

    access level changed

--- src/ir/baha2r/m98/crypto/aes/Holder.kt
+++ src/ir/baha2r/m98/crypto/aes/Holder.kt
@@ -1,6 +1,7 @@
 package ir.baha2r.m98.crypto.aes
 
-interface Holder {
+//TODO: add comments
+internal interface Holder {
     companion object {
         val IV: ByteArray = byteArrayOf(19, -53, -47, -48, -75, 18, 13, 19, 19, 15, 19, -55, -56, 10, 19, 9)
         val KEY: ByteArray = byteArrayOf(-55, 19, -51, -66, -63, -5, -2, 19, -47, -53, 19, -51, -53, -1, -75, -10)
